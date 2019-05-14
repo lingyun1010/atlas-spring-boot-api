f752ef555ac48eff7c193905e0a6f7b6046ab501
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-14 11:45:59 +0100

    Add a controller to retrieve all experiments list

---
+++ src/main/java/uk/ac/ebi/atlas/Controller.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.atlas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  @RequestMapping("/hello")
+  public String getHello() {
+    System.out.println("hello");
+    return "Hello World!";
+  }
+}
