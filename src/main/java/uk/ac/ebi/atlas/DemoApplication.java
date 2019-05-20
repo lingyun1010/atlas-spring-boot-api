fe04d7d8dc95d49ca625f271f61145473e3487ac
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-20 15:15:16 +0100

    Modify and slim spring boot configurations

--- src/main/java/uk/ac/ebi/atlas/DemoApplication.java
+++ src/main/java/uk/ac/ebi/atlas/DemoApplication.java
@@ -1,14 +1,9 @@
 package uk.ac.ebi.atlas;
 
-import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
@@ -18,9 +13,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	@Autowired
-	public SolrClient solrClient(final @Value("${solr.host:http://localhost:8983/solr/}") String solrHost) {
-		return new HttpSolrClient.Builder().withBaseSolrUrl(solrHost).build();
-	}
 }
