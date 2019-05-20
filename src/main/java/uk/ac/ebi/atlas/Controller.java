9bbd29e344e4c69f2ad03b70b05c7c4fccd9a56e
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-20 14:56:44 +0100

    Add a controller to retrieve sc json experiments

--- src/main/java/uk/ac/ebi/atlas/Controller.java
+++ src/main/java/uk/ac/ebi/atlas/Controller.java
@@ -1,13 +1,31 @@
 package uk.ac.ebi.atlas;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.atlas.experiments.ExperimentsListController;
+
+import javax.inject.Inject;
 
 @RestController
+@Scope("request")
 public class Controller {
-  @RequestMapping("/hello")
-  public String getHello() {
-    System.out.println("hello");
-    return "Hello World!";
+  private ExperimentsListController experimentsListController;
+
+  @Inject
+  public Controller(ExperimentsListController experimentsListController){
+    this.experimentsListController = experimentsListController;
+  }
+  @RequestMapping(value = "/api/sc/json/experiments",
+          method = RequestMethod.GET,
+          produces = "application/json;charset=UTF-8")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public String getExperimentsListFromSingleCell() {
+    return experimentsListController.getExperimentsListAPI();
   }
 }
